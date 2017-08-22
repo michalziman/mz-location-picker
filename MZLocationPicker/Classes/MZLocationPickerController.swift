87c6af6f97acd950c543761ec251681be845d9ab
Michal Ziman <ziman@reinto.cz>
2017-08-22 08:50:39 +0200

    Definitive decision on constraints and adds tap gesture recognizer to map view

--- MZLocationPicker/Classes/MZLocationPickerController.swift
+++ MZLocationPicker/Classes/MZLocationPickerController.swift
@@ -46,11 +46,29 @@ public class MZLocationPickerController: UIViewController {
         contentView.cancelButton.action = #selector(cancelPicking(_:))
         
         contentView.mapView.delegate = self
+        
+        let tapOnMap = UITapGestureRecognizer(target: self, action: #selector(setLocation(_:)))
+        tapOnMap.delegate = self
+        contentView.mapView.addGestureRecognizer(tapOnMap)
+        
+        let constraints = [
+            NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: contentView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0)
+        ]
+        view.addConstraints(constraints)
+        
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
     }
-
+    
     override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func hideKeyboard(){
+        view.endEditing(true)
     }
     
     func cancelPicking(_ sender: Any) {
@@ -58,6 +76,38 @@ public class MZLocationPickerController: UIViewController {
             d.locationPickerDidCancelPicking(self)
         }
         dismiss(animated: true)
+    }
+    
+    func setLocation(_ gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == .recognized {
+            let point = gestureRecognizer.location(in: locationPickerView.mapView)
+            let coordinates = locationPickerView.mapView.convert(point, toCoordinateFrom: locationPickerView.mapView)
+            
+            // add point annotation to map
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates
+            locationPickerView.mapView.addAnnotation(annotation)
+            
+//            let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+            // remember location, get address
+//            geocoder.cancelGeocode()
+//            geocoder.reverseGeocodeLocation(location) { response, error in
+//                if let error = error as NSError?, error.code != 10 {
+//                    self.location = Location(location: location)
+//                } else if let placemark = response?.first {
+//                    // get POI name from placemark if any
+//                    let name = placemark.areasOfInterest?.first
+//                    // pass user selected location too
+//                    self.location = Location(name: name, location: location, placemark: placemark)
+//                }
+//            }
+        }
+    }
+}
+
+extension MZLocationPickerController: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
--- MZLocationPicker/Classes/MZLocationPickerView.swift
+++ MZLocationPicker/Classes/MZLocationPickerView.swift
@@ -9,13 +9,26 @@
 import UIKit
 import MapKit
 
+fileprivate struct Constants {
+    static let shadowOpacity: Float = 0.3
+    static let bottomShadowOffset: CGSize = CGSize(width: 0, height: -3)
+    static let topShadowOffset: CGSize = CGSize(width: 0, height: 3)
+    static let shadowRadius: CGFloat = 2
+}
+
 class MZLocationPickerView: UIView {
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var navigationItem: UINavigationItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var chosenLocationView: UIView!
+    
+    @IBOutlet var showSearchConstraint: NSLayoutConstraint!
+    @IBOutlet var showChosenLocationConstraint: NSLayoutConstraint!
+    
     var isShowingLocateMe: Bool {
         set {
             navigationItem.leftBarButtonItem = newValue ? MKUserTrackingBarButtonItem(mapView: mapView) : nil
@@ -42,6 +55,23 @@ class MZLocationPickerView: UIView {
         nib.instantiate(withOwner: self, options: nil)
         
         contentView.frame = bounds
-        addSubview(contentView)        
+        addSubview(contentView)
+        
+        searchView.layer.masksToBounds = false
+        searchView.layer.shadowColor = UIColor.black.cgColor
+        searchView.layer.shadowOpacity = Constants.shadowOpacity
+        searchView.layer.shadowOffset = Constants.topShadowOffset
+        searchView.layer.shadowRadius = Constants.shadowRadius
+        
+        chosenLocationView.layer.masksToBounds = false
+        chosenLocationView.layer.shadowColor = UIColor.black.cgColor
+        chosenLocationView.layer.shadowOpacity = Constants.shadowOpacity
+        chosenLocationView.layer.shadowOffset = Constants.bottomShadowOffset
+        chosenLocationView.layer.shadowRadius = Constants.shadowRadius
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        showSearchConstraint.isActive = false
+        showChosenLocationConstraint.isActive = false
     }
 }
